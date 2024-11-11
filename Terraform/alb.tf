117e5800225f4eeebaba4c9d86cbe0fe4a332a93
ahmedjoubest <ahmedjoubest@gmail.com>
2024-11-12 00:50:24 +0100

    Create a different security group for ALB - Best Practice

--- Terraform/alb.tf
+++ Terraform/alb.tf
@@ -3,13 +3,40 @@ resource "aws_lb" "shiny_alb" {
   name               = "${var.env}-shiny-alb"  # Environment-specific name for easier management
   internal           = false                   # Set to false to make ALB internet-facing
   load_balancer_type = "application"           # Specifies ALB (Application Load Balancer)
-  security_groups    = [aws_security_group.ecs_security_group.id]  # Attach security group for traffic control
+  security_groups    = [aws_security_group.alb_security_group.id]  # Attach security group for traffic control
   subnets            = var.subnets             # Subnets in which to deploy the ALB
 
   enable_deletion_protection = false           # Disable deletion protection; adjust if needed for production
 }
 
-# 2. Create an ALB Listener with SSL certificate
+# 2. ALB Security Group
+resource "aws_security_group" "alb_security_group" {
+  name        = "${var.env}-alb-security-group"
+  description = "Security group for ALB to allow inbound HTTPS traffic"
+  vpc_id      = var.vpc_id
+
+  # Inbound rule to allow HTTPS access (port 443) from any IP
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs; restrict as needed for production security
+  }
+
+  # Outbound rule to allow all outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-alb-security-group"
+  }
+}
+
+# 3. Create an ALB Listener with SSL certificate
 resource "aws_lb_listener" "shiny_alb_listener" {
   load_balancer_arn = aws_lb.shiny_alb.arn     # Associate listener with the ALB created above
   port              = 443                      # Use HTTPS (port 443) for secure access
@@ -24,7 +51,7 @@ resource "aws_lb_listener" "shiny_alb_listener" {
   }
 }
 
-# 3. Define a Target Group for the ECS service
+# 4. Define a Target Group for the ECS service
 resource "aws_lb_target_group" "shiny_lb_target_group" {
   name        = "${var.env}-shiny-tg"          # Environment-specific name for the target group
   port        = var.container_port             # Use container port from variable (e.g., 3838)
